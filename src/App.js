cf9e2f83a106e3a1bf340511352acdabd7b4aeea
Rishi gupta <34707225+meetrishigupta@users.noreply.github.com>
2023-07-19 18:38:14 +0000

    ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Cards from "./Component/cards";
-
+import "./App.css"; // Import the CSS file
 function App() {
   return (
     <>
-      <h1 className="flex justify-center relative top-40 text-6xl font-medium ml-5 tracking-tight text-white	">
+      <h1 className="flex justify-center text-6xl font-medium ml-5 tracking-tight text-white custom-h1">
         Gradient Effect Hover Card
       </h1>
       <Cards />
     </>
   );
- 
 }
 
 export default App;
